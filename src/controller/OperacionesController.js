cda060828a37dc12c0019eea9ad6cc369647c3cb
jmcastilla <jmcastilla91@gmail.com>
2023-09-06 15:29:45 -0500

    prueba

--- src/controller/OperacionesController.js
+++ src/controller/OperacionesController.js
@@ -145,7 +145,7 @@ controller.get_contratostrafico = async (req, res) => {
             "d.Locked, c.Active, ISNULL(t.DistanciaReal,0) as DistanciaCompleta, t.Origen, d.FKLokTipoEquipo, "+
             "dbo.Tiempo(DATEDIFF(SECOND, LoksysServerTime, GETUTCDATE())) as Tiempo, DATEDIFF(SECOND, '1970-01-01 00:00:00', LoksysServerTime) as tiempoUnix, "+
             "dbo.iconbateria(ISNULL(ROUND(BatteryVoltage, 2),3)) as icon_bat, ROUND(BatteryVoltage, 2) as bateria, "+
-            "c.LastReportNota, tr.TipoReporte "
+            "c.LastReportNota, tr.TipoReporte "+
             "FROM LokcontractID as c "+
             "INNER JOIN LokDeviceID as d ON d.DeviceID = c.FKLokDeviceID "+
             "LEFT JOIN ICEmpresa as e ON e.IdEmpresa = c.FKICEmpresa "+
