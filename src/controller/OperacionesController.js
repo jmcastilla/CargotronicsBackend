980101c4e98651fb79a193625644d4d80ad90b5c
jmcastilla <jmcastilla91@gmail.com>
2023-09-07 17:25:43 -0500

    prueba

--- src/controller/OperacionesController.js
+++ src/controller/OperacionesController.js
@@ -151,8 +151,6 @@ controller.get_contratostrafico = async (req, res) => {
             "SUBSTRING(iconos.IconMoving, 2, CHARINDEX('|', iconos.IconMoving) - 2) AS IconMoving, "+
             "SUBSTRING(iconos.IconLocked, 2, CHARINDEX('|', iconos.IconLocked) - 2) AS IconLocked, "+
             "SUBSTRING(iconos.IconDesvio, 2, CHARINDEX('|', iconos.IconDesvio) - 2) AS IconDesvio, "+
-            //"SUBSTRING(iconos.IconSeparado, 2, CHARINDEX('|', iconos.IconSeparado) - 2) AS IconSeparado, "+
-            "1.png AS IconSeparado, "+
             "SUBSTRING(iconos.IconBack, 2, CHARINDEX('|', iconos.IconBack) - 2) AS IconBack "+
             "FROM LokcontractID as c "+
             "INNER JOIN LokDeviceID as d ON d.DeviceID = c.FKLokDeviceID "+
