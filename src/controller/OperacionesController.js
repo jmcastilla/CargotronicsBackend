fc9087f41c7db3be51c9e32449ca600b87fb6562
jmcastilla <jmcastilla91@gmail.com>
2023-09-11 17:26:19 -0500

    prueba

--- src/controller/OperacionesController.js
+++ src/controller/OperacionesController.js
@@ -147,7 +147,7 @@ controller.get_contratostrafico = async (req, res) => {
             "dbo.iconbateria(ISNULL(ROUND(BatteryVoltage, 2),3)) as icon_bat, ROUND(BatteryVoltage, 2) as bateria, "+
             "c.LastReportNota, tr.TipoReporte, c.LastReportUbica, "+
             "d.Ciudad+': '+d.Location+ CASE WHEN ContadorGps <> 0 THEN ' ('+CONVERT(NVARCHAR(20), ContadorGps)+')' ELSE '' END as Ciudad, "+
-            "DATEADD(MINUTE, DATEDIFF(INUTE, GETUTCDATE(), GETDATE)) + "+req.session.diffhorario+", LoksysServerTime) as LoksysServerTime, "+
+            "DATEADD(MINUTE, DATEDIFF(MINUTE, GETUTCDATE(), GETDATE)) + "+req.session.diffhorario+", LoksysServerTime) as LoksysServerTime, "+
             //"c.LastReportNota, tr.TipoReporte, (CASE d.Moving WHEN 1 THEN '/images/moving.png' ELSE '/images/stop.png' END) as IconMoving, "+
             //"(CASE d.Locked WHEN 1 THEN '/images/closedpadlock.png' ELSE '/images/openedpadlock.png' END) as IconLocked2, "+
             "SUBSTRING(iconos.IconMoving, 2, CHARINDEX('|', iconos.IconMoving) - 2) AS IconMoving, "+
