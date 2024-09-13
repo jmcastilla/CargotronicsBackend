a2c5447fa3266bd215ec7b81080d998b8428e41e
jmcastilla <jmcastilla91@gmail.com>
2024-09-13 15:08:32 -0500

    batery %

--- src/controller/OperacionesController.js
+++ src/controller/OperacionesController.js
@@ -228,7 +228,7 @@ controller.get_contratostrafico = async (req, res) => {
                     "ISNULL(CONVERT(varchar,DATEADD(minute,0,c.FechaHoraFin),20), CONVERT(varchar,DATEADD(minute,"+decoded.diffhorario+",GETDATE()),20)) as fechafin, c.LastMsgLat, c.LastMsgLong, "+
                     "d.Locked, c.Active, ISNULL(t.DistanciaReal,0) as DistanciaCompleta, t.Origen, d.FKLokTipoEquipo, "+
                     "dbo.Tiempo(DATEDIFF(SECOND, LoksysServerTime, GETUTCDATE())) as Tiempo, DATEDIFF(SECOND, '1970-01-01 00:00:00', LoksysServerTime) as tiempoUnix, "+
-                    "dbo.iconbateria(ISNULL(ROUND(BatteryVoltage, 2),3)) as icon_bat, ROUND(BatteryVoltage, 2) as bateria, "+
+                    "dbo.iconbateria2(ISNULL(ROUND(BatteryVoltage, 2),3), d.FKLokTipoEquipo) as icon_bat, ROUND(BatteryVoltage, 2) as bateria, "+
                     "c.LastReportNota, tr.TipoReporte, c.LastReportUbica+ ' ('+CONVERT(NVARCHAR(20), DATEDIFF(MINUTE, LastReportTime, DATEADD(MINUTE,"+decoded.diffhorario+", GETDATE())))+' min)' as LastReportUbica, "+
                     "d.Ciudad+': '+d.Location+ CASE WHEN ContadorGps <> 0 THEN ' ('+CONVERT(NVARCHAR(20), ContadorGps)+')' ELSE '' END as Ciudad, "+
                     "DATEADD(MINUTE, DATEDIFF(MINUTE, GETUTCDATE(), GETDATE()) + "+decoded.diffhorario+", LoksysServerTime) as LoksysServerTime, "+
