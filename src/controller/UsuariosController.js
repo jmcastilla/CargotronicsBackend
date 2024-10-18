2b2fadb1a16edf0307df649d39eea84ac5bf9386
jmcastilla <jmcastilla91@gmail.com>
2024-10-18 14:31:39 -0500

    lista ususarios por proyecto

--- src/controller/UsuariosController.js
+++ src/controller/UsuariosController.js
@@ -124,6 +124,7 @@ controller.get_listaRolusuarios = async (req, res) => {
                 } else {
                     var proyecto= req.body.proyecto;
                     var consulta= "SELECT IDRol, NombreRol FROM LokRoles ";
+                    console.log(decoded.tipouser+"   -    "+decoded.proyecto);
                     if(decoded.tipouser === 9){
                         if(decoded.proyecto === 9 || decoded.proyecto === 11){
                             consulta += " WHERE IDRol = 9 OR IDRol = 6 OR IDRol = 12 OR IDRol = 5 OR IDRol = 13 ";
