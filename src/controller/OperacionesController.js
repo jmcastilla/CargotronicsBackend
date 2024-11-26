dcf2069bfc1f825f527c6598a3a1687d51ec1ed9
jmcastilla <jmcastilla91@gmail.com>
2024-11-26 17:13:25 -0500

    notificaciones solicitudes

--- src/controller/OperacionesController.js
+++ src/controller/OperacionesController.js
@@ -27,7 +27,7 @@ controller.list_historicos = async (req, res) => {
             token = req.headers.authorization.split(' ')[1];
             jwt.verify(token, 'secret_key', async (err, decoded) => {
                 if (err) {
-                    res.json({ success: false, message: 'Failed to authenticate token' });
+                    return res.json({ success: false, message: 'Failed to authenticate token' });
                 } else {
                     var desde=req.body.desde;
                     var hasta=req.body.hasta;
@@ -51,9 +51,9 @@ controller.list_historicos = async (req, res) => {
                     }
                     let resultado=await sqlconfig.query(consulta);
                     if (resultado.recordsets && resultado.recordsets[0]) {
-  			               res.json({ success: true, data: resultado.recordsets[0] });
+  			               return res.json({ success: true, data: resultado.recordsets[0] });
                     } else {
-			                 res.json({ success: false, message: 'No data found' });
+			                 return res.json({ success: false, message: 'No data found' });
                     }
                 }
             });ss
