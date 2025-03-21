1e5fa475e331b0ee6a84f3aecae6568874d1e1ed
jmcastilla <jmcastilla91@gmail.com>
2025-03-21 12:34:26 -0500

    21032025

--- src/controller/OperacionesController.js
+++ src/controller/OperacionesController.js
@@ -504,7 +504,7 @@ controller.get_reportesdevice2 = async (req, res) => {
                     var datos={
                       "fechainicio": req.body.fechainicio,
                       //"fechafin":m.format('YYYY-MM-DD HH:mm:ss'),
-                      "fechafin":"2024-10-11 00:00:00",
+                      "fechafin":"2024-10-21 00:00:00",
                       "device": req.body.device,
                       "utcMinutos": decoded.diffUTC,
                       "allreport": req.body.allreport
