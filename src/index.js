437ea284b68f43ca1a627a80041cf6920e4fc8d9
jmcastilla <jmcastilla91@gmail.com>
2025-04-09 15:15:45 -0500

    notificaciones active

--- src/index.js
+++ src/index.js
@@ -932,6 +932,7 @@ const getTraficoGlobal = async () => {
 
 const getNotificacionesGlobal = async () => {
     var consulta = "SELECT TOP 1000 n.IdNotificacion, n.FkLokDeviceID, n.alertValue, n.idMensaje, n.DatetimeNoti, n.FkTipoNotificacion, "
+    +"n.NotificacionCliente, n.DatetimeEvento, n.AlertLevel, "
     +"n.FkLokContractID, n.FkUltGeoCerca, n.bitGeoAutorizada, n.Notificacion, n.FkLokProyecto, n.FkICEmpresa, n.FkIdAtencionNoti, '' as NombreEmpresa "
     +"FROM LokNotificaciones as n INNER JOIN LokcontractID as c ON n.FkLokContractID = c.ContractID where n.FkIdAtencionNoti is null AND c.Active=1";
     try {
